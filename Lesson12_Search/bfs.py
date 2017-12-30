4ae8627aa78c20aaa44e1fe2c23088e8403ac454
Justin Nguyen <2014justinnguyen@gmail.com>
2017-12-30 15:14:13 -0600

    added exansion list and shortest path soln

--- Lesson12_Search/bfs.py
+++ Lesson12_Search/bfs.py
@@ -12,15 +12,17 @@
 # 'fail'
 # ----------
 
+import numpy as np
+
 # Grid format:
 #   0 = Navigable space
 #   1 = Occupied space
 
-grid = [[0, 0, 1, 0, 0, 0],
-        [0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 0, 1, 0],
-        [0, 0, 1, 1, 1, 0],
-        [0, 0, 0, 0, 1, 0]]
+# grid = [[0, 0, 1, 0, 0, 0],
+#         [0, 0, 1, 0, 0, 0],
+#         [0, 0, 0, 0, 1, 0],
+#         [0, 0, 1, 1, 1, 0],
+#         [0, 0, 0, 0, 1, 0]]
 
 # grid = [[0, 0, 1, 0, 0, 0],
 #         [0, 0, 1, 0, 0, 0],
@@ -46,6 +48,18 @@ grid = [[0, 0, 1, 0, 0, 0],
 #         [0, 0, 1, 0, 1, 0],
 #         [0, 0, 1, 0, 1, 0]]
 
+# grid = [[0, 1, 1, 1, 1],
+#         [0, 1, 0, 0, 0],
+#         [0, 0, 0, 1, 0],
+#         [1, 1, 1, 1, 0],
+#         [0, 0, 0, 1, 0]]
+
+grid = [[0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 1, 0]]
+
 init = [0, 0]
 goal = [len(grid)-1, len(grid[0])-1]
 cost = 1
@@ -63,6 +77,28 @@ searched_already = [[0, 0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 0, 0]]
+
+# keep track of the order in which cells were searched
+expanded = [[-1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1]]
+
+# keep track of how many steps it took to reach each cell
+search_history = [[-1, -1, -1, -1, -1, -1],
+                  [-1, -1, -1, -1, -1, -1],
+                  [-1, -1, -1, -1, -1, -1],
+                  [-1, -1, -1, -1, -1, -1],
+                  [-1, -1, -1, -1, -1, -1]]
+
+# keep track of the shortest path
+path = [[' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ']]
+
 
 # made a new class to hold the tuple of information desired
 class BFS_El:
@@ -90,20 +126,27 @@ class BFS_El:
     def as_list(self):
         return [self.path_length, self.r, self.c]
 
-def valid_loc( el, grid ):
-    return el.r >= 0                               \
-            and  el.c >= 0                         \
-            and  el.r < len(grid)                  \
-            and  el.c < len(grid[0])               \
-            and  searched_already[el.r][el.c] == 0 \
-            and  grid[el.r][el.c] == 0             \
+def valid_loc( loc, grid ):
+    return loc[0] >= 0                                 \
+            and  loc[1] >= 0                           \
+            and  loc[0] < len(grid)                    \
+            and  loc[1] < len(grid[0])                 \
+            and  searched_already[loc[0]][loc[1]] == 0 \
+            and  grid[loc[0]][loc[1]] == 0             \
+            
+def valid_loc_reverse_path( loc, grid ):
+    return loc[0] >= 0                                 \
+            and  loc[1] >= 0                           \
+            and  loc[0] < len(grid)                    \
+            and  loc[1] < len(grid[0])                 \
+            and  grid[loc[0]][loc[1]] == 0             \
 
 def add_new_search_locs( search_list, cost ):
     for motion in delta:
         orig_el = search_list[0]
         new_el = BFS_El()
         new_el.set( orig_el.path_length+cost, orig_el.r+motion[0], orig_el.c+motion[1] )
-        if valid_loc( new_el, grid ):
+        if valid_loc( new_el.loc(), grid ):
             search_list.append( new_el )
             searched_already[ new_el.r][new_el.c ] = 1
 
@@ -111,16 +154,60 @@ def search(grid,init,goal,cost):
     init_el = BFS_El();
     init_el.set( 0, init[0], init[1] )
     search_list = [init_el]
+    expanded_counter = 0
     while True:
         if len(search_list) == 0:
+#             return expanded
             return 'fail'
         search_el = search_list[0]
         search_loc = search_el.loc()
         if search_loc == goal:
+            expanded[search_el.r][search_el.c] = expanded_counter
+            expanded_counter += 1
+            search_history[search_el.r][search_el.c] = search_el.path_length
+#             return expanded
             return search_el.as_list()
         else:
             add_new_search_locs( search_list, cost )
-            searched_already[ search_el.r][search_el.c ] = 1
+            searched_already[search_el.r][search_el.c] = 1
+            search_history[search_el.r][search_el.c] = search_el.path_length
+            expanded[search_el.r][search_el.c] = expanded_counter
+            expanded_counter += 1
             search_list.pop(0)
 
-print search( grid, init, goal, cost )
+def found_next_step( search_loc, min_steps ):
+    search_path_length = search_history[search_loc[0]][search_loc[1]]
+    return search_path_length == (min_steps - 1)
+
+def reverse_delta_name( delta_name ):
+    if delta_name == '^':
+        return 'v'
+    elif delta_name == '<':
+        return '>'
+    elif delta_name == 'v':
+        return '^'
+    elif delta_name == '>':
+        return '<'
+    else:
+        return None
+
+def get_path():
+    print np.array( search_history )
+    min_steps = search_history[goal[0]][goal[1]]
+    path[goal[0]][goal[1]] = '*'
+    cur_loc = goal
+    for i in range(min_steps):
+        for index,motion in enumerate(delta):
+            search_loc = [ cur_loc[0]+motion[0], cur_loc[1]+motion[1] ]
+            success = valid_loc(search_loc, grid)
+#             print; print("success: "); print(success);
+            if valid_loc_reverse_path(search_loc, grid) and found_next_step(search_loc, min_steps):
+                path[search_loc[0]][search_loc[1]] = reverse_delta_name( delta_name[index] )
+                cur_loc = search_loc
+                continue
+        min_steps -= 1
+    return path
+
+print np.array( search( grid, init, goal, cost ) )
+print np.array( get_path() )
+
